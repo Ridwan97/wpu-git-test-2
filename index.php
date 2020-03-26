cf04a9bf3aa2b15dfb66096ae98d8c30fe977088
Muhammad Ridwan <Ridwan.Sastrawijaya@gmail.com>
2020-03-27 04:40:29 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet culpa repudiandae ratione hic perspiciatis id iste molestias veniam unde rem autem architecto quo assumenda praesentium illum delectus, nobis quos reiciendis.</p>
+    <ul>
+    <li>satu</li>
+    <li>dua</li>
+    </ul>
 </body>
 </html>
